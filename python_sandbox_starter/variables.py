4035f60a9616a613e4cc0b96d6a74af7c018fbc7
Brian Lipsett <lipsettbrian3@gmail.com>
2021-11-10 12:52:21 -0700

    variable types

--- python_sandbox_starter/variables.py
+++ python_sandbox_starter/variables.py
@@ -13,3 +13,25 @@ VARIABLE RULES:
   - Must start with a letter or an underscore
   - Can have numbers but can not start with one
 """
+
+
+'''
+Main types of variables '''
+
+
+x = 1  #int 
+y = 2.5  #float
+name = 'john'  #string 
+is_cool = True # boolean
+
+# Multiple assignments
+x, y, name, is_cool = (1, 2, 'john', False)
+
+#Basic math
+a = x + y
+
+#casting 
+a = str(a)
+y = int(y)
+
+print(type(a))
