26189982cd806db29cb7d30ee0df96c69cad32e6
Brian Lipsett <lipsettbrian3@gmail.com>
2021-11-10 13:21:21 -0700

    lists strings

--- python_sandbox_starter/lists.py
+++ python_sandbox_starter/lists.py
@@ -1,1 +1,29 @@
 # A List is a collection which is ordered and changeable. Allows duplicate members.
+# MUCH LIKE ARRAYS IN JAVASCRIPT
+# 0 based
+
+# create list
+numbers = [1, 2, 3, 4]
+fruits = ['apples', 'oranges', 'grapes', 'pears']
+
+#Use a constructor 
+numbers2 = list((1, 2, 3, 4, 5))
+
+print(fruits[1])
+
+print(len(fruits))
+
+fruits.append('mangoes')
+
+fruits.remove('pears')
+
+fruits.insert(1, 'strawberries')
+
+fruits.pop(2)
+
+fruits.reverse()
+
+fruits.sort()
+
+fruits[0] = 'blueberries'
+print(fruits)
--- python_sandbox_starter/strings.py
+++ python_sandbox_starter/strings.py
@@ -1,6 +1,19 @@
 # Strings in python are surrounded by either single or double quotation marks. Let's look at string formatting and some string methods
+name = 'brian'
+age = 29
 
 
 # String Formatting
+print('Hello, my name is ' + name + ', I am ' + str(age))
+
+
+# Arguments by position
+print('My name is {name} and I am {age}'.format(name=name, age=age))
+
+# F strings
+print(f'Hello my name is {name} and i am {age}')
+
+s = "hello world"
 
 # String Methods
+print(s.capitalize())
