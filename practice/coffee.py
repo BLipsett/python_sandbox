33ba52fbb6b24912eaaf44f4ea30cb9a3db3a2f4
Brian Lipsett <lipsettbrian3@gmail.com>
2021-11-16 21:14:01 -0700

    coffee prompt

--- practice/coffee.py
+++ practice/coffee.py
@@ -29,3 +29,29 @@ resources = {
     "milk": 200,
     "coffee": 100,
 }
+
+def checkSource(user_Choice):
+  print(user_Choice)
+  
+
+
+#TODO ask user for drink order
+
+def start_Order():
+  user_choice = input("What would you like? (espresso/latte/cappuccino):")
+  print("you chose " + user_choice)
+
+  if user_choice != "espressso" or "latte" or "cappuccino": 
+    print("please make a choice from the list")
+  else:
+    print("take a " + user_choice)
+    # start_Order()
+
+
+start_Order()
+#TODO check resources to make drink type 
+# checkSource(coffee)
+
+#TODO reply with cost 
+
+#TODO check user
